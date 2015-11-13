4b098a98364555b16ea68cadffb3a6938f49b685
Marc Altmann <mail@marcaltmann.com>
2015-11-13 20:27:57 +0100

    Add validation test for Invoice

--- spec/entities/invoice_spec.rb
+++ spec/entities/invoice_spec.rb
@@ -1,20 +1,23 @@
 require 'entities/invoice.rb'
 
 describe Invoice do
-  let(:invoice) do
-    Invoice.new(customer_name: 'Franz Beckenbauer', service: 'something', amount: 9.95,
-                date: Date.today)
-  end
+  let(:invoice) { Invoice.new(customer_name: 'Franz Beckenbauer', service: 'something', amount: 9.95, date: Date.today) }
+  let(:invalid_invoice) { Invoice.new(customer_name: nil, service: nil, amount: nil, date: nil) }
 
   it 'can create a new invoice' do
     invoice
   end
 
-  it 'can validate an invoice' do
+  it 'can persist an invoice' do
+    invoice.persist
+  end
+
+  # here we need some validation tests!!
+  it 'valid invoice should be validated' do
     invoice.validate
   end
 
-  it 'can persist an invoice' do
-    invoice.persist
+  it 'invalid invoice validation should throw an error' do
+    expect { invalid_invoice.validate }.to raise_error
   end
 end
