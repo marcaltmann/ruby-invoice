104954ce24e2912c927b64b04024d20a7a783b3a
Marc Altmann <mail@marcaltmann.com>
2015-11-13 20:21:08 +0100

    Refactor CreateInvoice

--- control_objects/create_invoice.rb
+++ control_objects/create_invoice.rb
@@ -1,13 +1,17 @@
 require_relative '../entities/invoice'
 
 class CreateInvoice
-  def initialize(output_object, invoice_attributes)
+  def initialize(output_object, customer_name:, service:, amount:, date:)
     @output_object = output_object
-    @invoice_attributes = invoice_attributes
+    @customer_name = customer_name
+    @service = service
+    @amount = amount
+    @date = date
   end
 
   def perform
-    @invoice = Invoice.new(@invoice_attributes)
+    @invoice = Invoice.new(customer_name: @customer_name, service: @service,
+                           amount: @amount, date: @date)
     begin
       @invoice.validate
       @invoice.persist
