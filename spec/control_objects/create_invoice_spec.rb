33aa814a432acadf78d4ddf1629b004a312e959b
Marc Altmann <mail@marcaltmann.com>
2015-11-13 19:47:22 +0100

    Fix tests

--- spec/control_objects/create_invoice_spec.rb
+++ spec/control_objects/create_invoice_spec.rb
@@ -1,11 +1,14 @@
 require 'control_objects/create_invoice'
 
 describe CreateInvoice do
+  let(:dbl) { instance_double(Invoice, validate: nil, persist: nil) }
+  let(:output) { double(OutputObject, success: nil, failure: nil) }
+
   it 'should create a new Invoice entity' do
-    dbl = instance_double(Invoice, validate: nil, persist: nil)
     allow(Invoice).to receive(:new).and_return(dbl)
 
-    invoice_creator = CreateInvoice.new(customer_name: 'Franz Beckenbauer', service: 'something',
+    invoice_creator = CreateInvoice.new(output,
+                                        customer_name: 'Franz Beckenbauer', service: 'something',
                                         amount: 9.95, date: Date.today)
     invoice_creator.perform
 
@@ -14,11 +17,11 @@ describe CreateInvoice do
   end
 
   it 'should raise an error if Invoice entity is invalid' do
-    dbl = instance_double(Invoice, validate: nil, persist: nil)
     allow(Invoice).to receive(:new).and_return(dbl)
     allow(dbl).to receive(:validate).and_raise('not correct')
 
-    invoice_creator = CreateInvoice.new(customer_name: nil, service: nil,
+    invoice_creator = CreateInvoice.new(output,
+                                        customer_name: nil, service: nil,
                                         amount: nil, date: nil)
     invoice_creator.perform
   end
